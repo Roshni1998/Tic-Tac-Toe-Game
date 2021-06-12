6f2a5e6788a069374db9c2c113dc487eac01d48a
Roshni1998 <roshnimali847@gmail.com>
2021-06-12 16:19:38 +0530

    Tic-Tac-Toe UC1

--- TicTacToeGame.java
+++ TicTacToeGame.java
@@ -8,5 +8,18 @@ public class TicTacToeGame {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to TicTacToe Game");
+		TicTacToeGame game = new TicTacToeGame();
+		game.createTheBoard();
+	}
+
+	/* UC 1 */
+	// Creating board
+	char board[] = new char[9];
+
+	public void createTheBoard() {
+		for (int i = 1; i < 9; i++) {
+			board[i] = ' ';
+			System.out.println(board[i]);
+		}
 	}
 }
