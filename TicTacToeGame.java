dead5e728c49bc57bf3b77099a78ba72ee948ad2
Roshni1998 <roshnimali847@gmail.com>
2021-06-13 15:50:08 +0530

    Added: Tic-Tac-Toe UC-4

--- TicTacToeGame.java
+++ TicTacToeGame.java
@@ -7,55 +7,40 @@ import java.util.Scanner;
  */
 
 public class TicTacToeGame {
-	
-	Scanner op = new Scanner(System.in);
-	public char player = op.next().charAt(0);
-	public char computer = ' ';
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to TicTacToe Game");
-		System.out.println("Select X or O as your marker");
-		TicTacToeGame game = new TicTacToeGame();
-		game.createTheBoard();
-		game.chooseMarker();
-		game.showBoard();
+		char[] board = TicTacToeGame.createTheBoard();
+		Scanner userInput = new Scanner(System.in);
+		char player = chooseMarker(userInput);
+		char computer = (player == 'X') ? 'O' : 'X';
+		showBoard(board);
+		System.out.println("Player Marker : " + player);
+		System.out.println("Computer's Marker : " + computer);
+		playerChoice(board, player);
 	}
 
 	/* UC 1 */
 	// Creating board
-	char board[] = new char[10];
-
-	public void createTheBoard() {
+	public static char[] createTheBoard() {
+		char board[] = new char[10];
 		for (int i = 1; i < 10; i++) {
-			board[i] = ' ';
-			System.out.println(board[i]);
+			board[i] = '_';
 		}
+		return board;
 	}
-	
+
 	/* UC 2 */
 	// Provide player a choice to choose his marker
-	public void chooseMarker() {
-		// System.out.println("Select X or O as your marker");
-		if (player == 'X') {
-			computer = 'O';
-			System.out.println("Player1 Choice : " + player);
-			System.out.println("Computer Choice : " + computer);
-
-		}
-		else if (player == 'O') {
-			computer = 'X';
-			System.out.println("Player1 Choice : " + player);
-			System.out.println("Computer Choice : " + computer);
-
-		} else {
-			System.out.println("Please Enter valid Choice! ");
-		}
+	private static char chooseMarker(Scanner userInput) {
+		System.out.println("Select X or O as your marker");
+		return userInput.next().toUpperCase().charAt(0);
 	}
-	
-	/* UC 3
-	*Showing the board to the player
-	*/
-	public void showBoard() {
+
+	/*
+	 * UC 3 Showing the board to the player
+	 */
+	public static void showBoard(char[] board) {
 		System.out.println(" |---|---|---|");
 		System.out.println(" | " + board[1] + " | " + board[2] + " | " + board[3] + " | ");
 		System.out.println(" |---|---|---|");
@@ -64,5 +49,21 @@ public class TicTacToeGame {
 		System.out.println(" | " + board[7] + " | " + board[8] + " | " + board[9] + " | ");
 		System.out.println(" |---|---|---|");
 	}
-}
 
+	/*
+	 * UC 4 Ability for user to make a move to a desired location
+	 */
+
+	public static void playerChoice(char[] board, char player) {
+		System.out.println("Please make your choice between 1 to 9");
+		Scanner ch = new Scanner(System.in);
+		int choice = ch.nextInt();
+		if (board[choice] == '_') {
+			board[choice] = player;
+			showBoard(board);
+		} else {
+			System.out.println("Someone has already made a move here!!");
+		}
+	}
+
+}
